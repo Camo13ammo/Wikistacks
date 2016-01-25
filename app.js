9629863d1aa1e69c1ca1bde22f2491b3f90fac42
Sean Colandrea <Sean@Seans-MacBook-Pro.local>
2016-01-25 18:28:19 -0500

    End of class commit

---
+++ app.js
@@ -0,0 +1,24 @@
+'use strict';
+
+var express = require('express');
+var app = express();
+var routes = require('./routes');
+var morgan = require('morgan');
+var swig = require('swig');
+var path = require('path');
+
+// Initialize SWIG
+app.set('views', __dirname + '/views');
+app.set('view engine', 'html');
+app.engine('html', swig.renderFile);
+swig.setDefaults({cache: false});
+
+app.use(morgan('dev'));
+
+app.use("/", routes);
+
+app.use(express.static(path.join(__dirname,'/public')));
+
+app.listen(3000, function() {
+	console.log('Listening on port 3000');
+});
